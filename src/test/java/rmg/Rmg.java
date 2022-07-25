629c7c8fb0f4ea596c048c764378f2e072bf4b15
meeth03 <meeth0304yadav@gmail.com>
2022-07-25 13:05:11 +0530

    comit8

---
+++ src/test/java/rmg/Rmg.java
@@ -0,0 +1,11 @@
+package rmg;
+
+public class Rmg {
+	public static void main(String[]args){
+		System.out.println("hello");
+		System.out.println("hey");
+		System.out.println("hello");
+		System.out.println("hey");
+	}
+
+}
